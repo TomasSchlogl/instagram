9d468930b4c6574b0192338ed3b2f6943876be2c
TomasSchlogl <thomas537@seznam.cz>
2021-07-20 21:16:30 +0200

    signin done

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import "./App.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import * as ROUTES from "./constants/routes";
 import { lazy, Suspense } from "react";
+import SignUp from "./pages/signup";
 
 const Login = lazy(() => import("./pages/login"));
+const SignUp = lazy(() => import("./pages/sign-up"));
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
       <Suspense fallback={<p>loading..</p>}>
         <Switch>
           <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGN_UP} component={SignUp} />
         </Switch>
       </Suspense>
     </Router>
